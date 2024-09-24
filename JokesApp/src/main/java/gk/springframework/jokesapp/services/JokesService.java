d23baaa4fc9e188a538f7f6d7238ef326f3142db
juluri.krishna <juluri.krishna@ivyvomptech.com>
2024-09-24 20:08:40 +0530

    Added JokesService

---
+++ JokesApp/src/main/java/gk/springframework/jokesapp/services/JokesService.java
@@ -0,0 +1,7 @@
+package gk.springframework.jokesapp.services;
+
+import org.springframework.stereotype.Service;
+
+public interface JokesService{
+    String getJoke();
+}
---
+++ JokesApp/src/main/java/gk/springframework/jokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,18 @@
+package gk.springframework.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
